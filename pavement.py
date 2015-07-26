73a13b2948dbf2321349209d83958c913225b82e
Brad Miller <bonelake@mac.com>
2015-07-26 11:39:20 -0500

    changes for consistency with automatic build and fixes for new components
    Conflicts:
    	_sources/Assignments/ps1.rst
    	_sources/Assignments/ps12.rst
    	_sources/Assignments/ps2.rst
    	_sources/Assignments/ps3.rst
    	_sources/Assignments/ps4.rst
    	_sources/Assignments/ps5.rst
    	_sources/Assignments/ps6.rst
    	_sources/Assignments/ps8.rst
    	_sources/Assignments/ps9.rst
    	_sources/Assignments/session21.rst
    	_sources/Assignments/week11.rst
    	_sources/Assignments/week8.rst
    	_sources/RESTAPIs/FBAPI.rst
    	_sources/RESTAPIs/flickr.rst
    	_sources/Selection/BooleanValuesandBooleanExpressions.rst
    	_sources/Selection/Logicaloperators.rst
    	_sources/Selection/PrecedenceofOperators.rst
    	_sources/SimplePythonData/BooleanValuesandBooleanExpressions.rst
    	_sources/SimplePythonData/Logicaloperators.rst
    	_sources/SimplePythonData/PrecedenceofOperators.rst
    	source/Assignments/ps12.rst
    	source/Assignments/ps9.rst
    	source/Assignments/session21.rst
    	source/Selection/BooleanValuesandBooleanExpressions.rst
    	source/Selection/Logicaloperators.rst
    	source/Selection/PrecedenceofOperators.rst

--- pavement.py
+++ pavement.py
@@ -2,19 +2,6 @@ import paver
 from paver.easy import *
 import paver.setuputils
 paver.setuputils.install_distutils_tasks()
-import os, sys
-
-from sphinxcontrib import paverutils
-
-sys.path.append(os.getcwd())
-sys.path.append('../modules')
-
-updateProgressTables = True
-try:
-    from chapternames import populateChapterInfo
-except Exception as e:
-    print e
-    updateProgressTables = False
 
 
 ######## CHANGE THIS ##########
@@ -29,54 +16,20 @@ options(
 
     build = Bunch(
         builddir="./build/"+project_name,
-        sourcedir="source/",
+        sourcedir="./_sources/",
         outdir="./build/"+project_name,
         confdir=".",
-        template_args={'course_id':project_name,
-                       'login_required':'false',
-                       'appname':master_app,
-                       'loglevel':10,
-                       'course_url':master_url }
+        project_name = project_name,
+        template_args = {
+            'course_id':project_name,
+            'login_required':'false',
+            'appname':master_app,
+            'loglevel':10,
+            'course_url':master_url,
+            'use_services': 'true',
+            'python3': 'true',
+        }
     )
 )
 
-if project_name == "<project_name>":
-  print "Please edit pavement.py and give your project a name"
-  exit()
-
-@task
-@cmdopts([
-    ('all','a','rebuild everything'),
-    ('outputdir=', 'o', 'output static files here'),
-    ('masterurl=', 'u', 'override the default master url'),
-    ('masterapp=', 'p', 'override the default master app')
-])
-def build(options):
-    sh('cp %s/index.rst %s' % (options.build.confdir,options.build.sourcedir))
-
-    if 'all' in options.build:
-      options['force_all'] = True
-      options['freshenv'] = True
-
-    try:
-        bi = sh('git describe --long',capture=True)[:-1]
-        bi = bi.split('-')[0]
-        options.build.template_args["build_info"] = bi
-    except:
-        options.build.template_args["build_info"] = 'unknown'
-
-    if 'outputdir' in options.build:
-        options.build.outdir = options.build.outputdir
-
-    if 'masterurl' in options.build:
-        options.build.template_args['course_url'] = options.build.masterurl
-
-    if 'masterapp' in options.build:
-        options.build.template_args['appname'] = options.build.masterapp
-
-    print 'Building into ', options.build.outdir    
-    paverutils.run_sphinx(options,'build')
-
-    if updateProgressTables:
-        print 'Creating Chapter Information'
-        populateChapterInfo(project_name, "%s/toc.rst" % options.build.sourcedir)
+from runestone import build  # build is called implicitly by the paver driver.
