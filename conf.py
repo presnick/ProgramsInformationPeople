c5e3cb503806aa051e6424b10c80e7494cd2d666
Paul Resnick <presnick@umich.edu>
2015-09-01 15:18:57 -0400

    moved files to top-level from subfolder pip2

--- conf.py
+++ conf.py
@@ -16,8 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-
-from runestone import runestone_static_dirs, runestone_extensions
+sys.path.insert(0, os.path.abspath('../modules'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -26,14 +25,14 @@ from runestone import runestone_static_dirs, runestone_extensions
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax'] + runestone_extensions()
+extensions = ['sphinx.ext.mathjax','luther.sphinx.disqus','luther.sphinx.reveal','luther.sphinx.poll','luther.sphinx.video','luther.sphinx.codelens','luther.sphinx.activecode','luther.sphinx.tabbedStuff', 'luther.sphinx.assess', 'luther.sphinx.animation','luther.sphinx.meta', 'gatech.parsons', 'luther.sphinx.datafile']
 
 # Make it use https version of mathjax
 mathjax_path = "https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['../source/_templates']
-templates_path = ['./_sources/_templates']
+templates_path = ['./source/_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -150,7 +149,7 @@ html_theme_options = {
 #html_style = "style.css"
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["./_sources/_templates/plugin_layouts"]
+html_theme_path = ["./source/_templates/plugin_layouts"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -172,7 +171,12 @@ html_short_title = 'Programs, Information, and People'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-html_static_path = ['./_sources/_static']  + runestone_static_dirs()
+module_paths = [x.replace('.','/') for x in extensions]
+module_static_js = ['../modules/%s/js' % x for x in module_paths if os.path.exists('../modules/%s/js' % x)]
+module_static_css = ['../modules/%s/css' % x for x in module_paths if os.path.exists('../modules/%s/css' % x)]
+
+html_static_path = ['./source/_static', '../common/js', '../common/css',
+                    '../common/bootstrap', '../common/images'] + module_static_js + module_static_css
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
