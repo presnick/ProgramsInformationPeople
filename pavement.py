d94b1a9f3f641ae921fd02565b79e637df67c9f5
Stephen Oney <swloney@gmail.com>
2017-09-04 09:58:07 -0400

    changing default url

--- pavement.py
+++ pavement.py
@@ -10,7 +10,7 @@ project_name = "pip2"
 
 # master_url = 'http://127.0.0.1:8000'
 # master_url = 'https://www.programsinformationpeople.org'
-master_url = 'https://506f17.learningpython.today'
+master_url = 'https://106f17.learningpython.today'
 master_app = 'runestone'
 serving_dir = "./build/" + project_name
 dest = "../../static"
